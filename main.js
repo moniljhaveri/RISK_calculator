0069d546951a6a23a8053eefb359a3c2c3167adb
moniljhaveri <mjhaveri@bu.edu>
2017-12-19 18:08:14 -0500

    created a js file for rendering main page and doing main page logic

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
-let win 
 
 function createWindow() {
     win = new BrowserWindow({width: 800, height:600})
